cdeebefed7521a019624bd3d55e4a50c3029b539
Manuela Puerta <Manuela.puertaa@uqvirtual.edu.co>
2025-11-17 22:21:47 -0500

    funciones del servicio mentoria

--- services/servicio_mentoria.ex
+++ services/servicio_mentoria.ex
@@ -17,3 +17,114 @@ defmodule Servicios.ServicioMentoria do
     Process.register(pid, @nombre_servicio)
     {:ok, pid}
   end
+
+  defp registrar_mentor(nombre, correo, especialidad) do
+    case Almacenamiento.obtener_mentor(nombre) do
+      nil ->
+        case Mentor.nuevo(nombre, correo, especialidad) do
+          {:ok, mentor} ->
+            Almacenamiento.guardar_mentor(mentor)
+            {:ok, mentor}
+
+          {:error, mensaje} ->
+            {:error, mensaje}
+        end
+
+      _mentor ->
+        {:error, "Ya existe un mentor con ese nombre"}
+    end
+  end
+
+  defp asignar_equipo_a_mentor(nombre_mentor, nombre_equipo) do
+    case Almacenamiento.obtener_mentor(nombre_mentor) do
+      nil ->
+        {:error, "Mentor no encontrado"}
+
+      mentor ->
+        # Verificar que el equipo existe
+        case Almacenamiento.obtener_equipo(nombre_equipo) do
+          nil ->
+            {:error, "El equipo no existe"}
+
+          _equipo ->
+            case Mentor.asignar_equipo(mentor, nombre_equipo) do
+              {:ok, mentor_actualizado} ->
+                Almacenamiento.guardar_mentor(mentor_actualizado)
+                {:ok, "Equipo asignado al mentor"}
+
+              {:error, msg} ->
+                {:error, msg}
+            end
+        end
+    end
+  end
+
+  defp dar_retroalimentacion(nombre_mentor, nombre_equipo, comentario) do
+    case Almacenamiento.obtener_mentor(nombre_mentor) do
+      nil ->
+        {:error, "Mentor no encontrado"}
+
+      mentor ->
+        # Verificar que el equipo está asignado al mentor
+        if Mentor.tiene_equipo?(mentor, nombre_equipo) do
+          # Obtener el proyecto y agregar retroalimentación
+          case Almacenamiento.obtener_proyecto(nombre_equipo) do
+            nil ->
+              {:error, "No existe un proyecto para ese equipo"}
+
+            proyecto ->
+              # Agregar retroalimentación al proyecto
+              proyecto_actualizado = Dominio.Proyecto.agregar_retroalimentacion(
+                proyecto,
+                nombre_mentor,
+                comentario
+              )
+              Almacenamiento.guardar_proyecto(proyecto_actualizado)
+              {:ok, "Retroalimentación enviada al equipo"}
+          end
+        else
+          {:error, "Este equipo no está asignado a tu mentoría"}
+        end
+    end
+  end
+
+  @doc """
+  Solicita registrar un mentor
+  """
+  def solicitar_registrar(nombre, correo, especialidad) do
+    send(@nombre_servicio, {self(), :registrar, nombre, correo, especialidad})
+
+    receive do
+      {:mentor_registrado, resultado} -> resultado
+    after
+      5000 -> {:error, "Timeout"}
+    end
+  end
+
+  @doc """
+  Solicita listar mentores
+  """
+  def solicitar_listar() do
+    send(@nombre_servicio, {self(), :listar})
+
+    receive do
+      {:lista_mentores, mentores} -> mentores
+    after
+      5000 -> []
+    end
+  end
+
+  @doc """
+  Solicita asignar un equipo a un mentor
+  """
+  def solicitar_asignar_equipo(nombre_mentor, nombre_equipo) do
+    send(@nombre_servicio, {self(), :asignar_equipo, nombre_mentor, nombre_equipo})
+
+    receive do
+      {:equipo_asignado, resultado} -> resultado
+    after
+      5000 -> {:error, "Timeout"}
+    end
+  end
+end
+
