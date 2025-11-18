9f0e447e4284069cadc2b635c39100aa5396b7c8
Manuela Puerta <Manuela.puertaa@uqvirtual.edu.co>
2025-11-17 22:16:18 -0500

    funciones del chat
    solicitar obtener

--- services/servicio_chat.ex
+++ services/servicio_chat.ex
@@ -16,3 +16,58 @@ defmodule Servicios.ServicioChat do
     Process.register(pid, @nombre_servicio)
     {:ok, pid}
   end
+#funciones privadas
+
+  defp guardar_mensaje(canal, autor, texto) do
+    mensaje = %{
+      canal: canal,
+      autor: autor,
+      texto: texto,
+      timestamp: DateTime.utc_now()
+    }
+
+    Almacenamiento.guardar_mensaje(mensaje)
+    {:ok, "Mensaje enviado"}
+  end
+
+  # api
+
+  @doc """
+  Solicita enviar un mensaje a un canal
+  """
+  def solicitar_enviar_mensaje(canal, autor, texto) do
+    send(@nombre_servicio, {self(), :enviar_mensaje, canal, autor, texto})
+
+    receive do
+      {:mensaje_enviado, resultado} -> resultado
+    after
+      5000 -> {:error, "Timeout"}
+    end
+  end
+
+  @doc """
+  Solicita obtener mensajes de un canal
+  """
+  def solicitar_obtener_mensajes(canal) do
+    send(@nombre_servicio, {self(), :obtener_mensajes, canal})
+
+    receive do
+      {:mensajes, mensajes} -> mensajes
+    after
+      5000 -> []
+    end
+  end
+
+  @doc """
+  Envia un anuncio general
+  """
+  def solicitar_anuncio_general(texto) do
+    send(@nombre_servicio, {self(), :anuncio_general, texto})
+
+    receive do
+      {:anuncio_enviado, resultado} -> resultado
+    after
+      5000 -> {:error, "Timeout"}
+    end
+  end
+end
